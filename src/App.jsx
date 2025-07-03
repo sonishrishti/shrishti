7d0b8601647c13267a47ceb8061ec4545329f3c1
Shrishti Soni <soni.shrishti@yahoo.com>
2025-07-03 13:20:11 +0100

    Progressing slowly. Made some changes to the timeline and changed some colors.

--- src/App.jsx
+++ src/App.jsx
@@ -2,38 +2,53 @@ import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timel
 import 'react-vertical-timeline-component/style.min.css';
 import './App.css'
 import timelineElements from './timelineElements';
+import { FaLaptopCode } from "react-icons/fa";
+import { PiStudentFill } from "react-icons/pi";
+import { PiHandsPrayingFill } from "react-icons/pi";
+
 
 function App() {
-  let IconStyle = {background: "white"}
+  let IconStyle = {background: "rgb(255, 153, 1)"};
+  let workIconimg = <FaLaptopCode />;
+  let eduIconimg = <PiStudentFill />;
+  let namaste = <PiHandsPrayingFill />;
 
   return (
     <>
       <div className="app-container">
         <div className="intro">
-          <h1>Jai Shri Krishna</h1>
+          <h1>Jai Shri Krishna {namaste}</h1>
           <p className="name">I am Shrishti Soni.</p>
+          <p className = "sub">and you are entering my world!</p>
         </div>
       </div>
 
       <div className="second-section">
         <div className="education">
-          <h1>Education</h1>
-          {/*Outer {} tells React you’re injecting JavaScript. 
-          Inner { textAlign: "center" } is the actual JavaScript object */}
+          <h1>My life</h1>
           <VerticalTimeline>
             {
               timelineElements.map(element => {
+                let isworkIcon = element.icon === "work";
                 return (
                   <VerticalTimelineElement
+                    contentStyle={{ background: '#F5F5DC'}}
                     key={element.key}
                     date={element.date}
                     dateClassName="date"
                     iconStyle={IconStyle}
+                    icon = {isworkIcon? workIconimg : eduIconimg}
                   >
                     <h3 className="vertical-timeline-element-title">{element.name}</h3>
+                    <h4 className="vertical-timeline-element-subtitle">
+                      <i>{element.course}</i>
+                    </h4>
                     <h5 className="vertical-timeline-element-subtitle">
                       {element.location}
                     </h5>
+                    <p id="description">
+                      {element.description}
+                    </p>
 
                   </VerticalTimelineElement>
                 )
@@ -41,14 +56,13 @@ function App() {
             }
           </VerticalTimeline>
         </div>
-        <div className = "workex">
-          max
-        </div>
+        
       </div>
+
         
 
       <div className = "aboutme">
-        <h1>About me</h1>
+        <h1>What do I like apart from my Lala?</h1>
       </div>
     </>
 
--- src/timelineElements.js
+++ src/timelineElements.js
@@ -4,6 +4,8 @@ let timelineElements = [
         name: "Shirin Dinyar Irani Learners Academy",
         course: "Schooling till 10th",
         location: "Dahanu Road, India",
+        description: "",
+        icon: "edu",
         date: "Till 2018"
     },
     {
@@ -11,22 +13,82 @@ let timelineElements = [
         name: "Shri Bhagubhai Mafatlal Polytechnic",
         course: "High School Diploma in Computer Engineering",
         location: "Mumbai, India",
+        description: "",
+        icon: "edu",
         date: "2018 - 2021"
     },
     {
         id: 3,
-        name: "Atharva College Of Engineering",
-        course: "BE Computer Engineering",
-        location: "Mumbai, India",
-        date: "2021 - 2024"
+        name: "DivyDishaa",
+        course: "Founder",
+        location: "Dahanu Road, India",
+        description: "My first baby venture!! With my Lala's blessings, I made this place to put out my thoughts. You ask _why?_; well, because, I believed my writeups deserved to be read!",
+        icon: "work",
+        date: "2019 - Present"
     },
     {
         id: 4,
+        name: "Whitehat Jr.",
+        course: "Coding Instructor",
+        location: "Dahanu Road, India",
+        description: "I got the chance to connect with students from around the globe and be a part of their coding journey.",
+        icon: "work",
+        date: "2020 - 2023"
+    },
+    {
+        id: 5,
+        name: "Atharva College Of Engineering",
+        course: "BE Computer Engineering",
+        location: "Mumbai, India",
+        description: "",
+        icon: "edu",
+        date: "2021 - 2024"
+    },
+    {
+        id: 6,
+        name: "Hiral Farms",
+        course: "AR Developer Intern",
+        location: "Mumbai, India",
+        description: "Heard of AR? Imagine seeing dinosaurs while walking through a farm. Those big creatures wagging their tails, looking at you. Hmmm! Yes, this is what I did here!",
+        icon: "work",
+        date: "2023 - 2024"
+    },
+    {
+        id: 7,
         name: "University College Dublin",
         course: "MSc Computer Science",
         location: "Dublin, Ireland",
-        date: "2024-2025"
-    }
+        description: "",
+        icon: "edu",
+        date: "2024 - 2025"
+    },
+    {
+        id: 8,
+        name: "UCD",
+        course: "Peek-a-card",
+        location: "Dublin, Ireland",
+        description: "Want a perfect gift for your loved ones? Let that be friends, family, colleagues, we have something special for everyone. Just say it and we will do it! In this joint venture with my friend, I gave messages voice and brought ideas to life!",
+        icon: "work",
+        date: "2025"
+    },
+    {
+        id: 9,
+        name: "UCD",
+        course: "Took the bait?",
+        location: "Dublin, Ireland",
+        description: "I always wanted to have my own radio show and when I got the chance, I grabbed it just like a baby grabs a choco! He he he! 'Took the bait?' is that radio show!! ",
+        icon: "work",
+        date: "2025"
+    },
+    {
+        id: 10,
+        name: "The Dyslexia Specialist",
+        course: "IT Support",
+        location: "Dublin, Ireland",
+        description: "I am responsible for everything that Catherine asks me to do :). From course developemnt to social media marketing, I am the one stop person for everything that involves tech! (even little)!",
+        icon: "work",
+        date: "2025 - Present"
+    },
 ];
 
 export default timelineElements;
